e78e3a4b56200eabd53f1db675492ef6d729fae7
xp9477 <947760133@qq.com>
2024-12-12 15:32:24 +0800

    .

--- rules/Loon/script/yangjibao.js
+++ rules/Loon/script/yangjibao.js
@@ -8,25 +8,15 @@
 
 [rewrite_local]
 
-^https:\/\/app-api\.yangjibao\.com\/account url script-response-body https://raw.githubusercontent.com/xp9477/Scripts/main/rules/Loon/script/yangjibao.js
+^https?:\/\/.*yangjibao\.com\/(wxapi\/)?account url script-response-body https://github.com/xp9477/Scripts/raw/main/rules/Loon/script/yangjibao.js
 
 [mitm] 
 
-hostname = app-api.yangjibao.com
+hostname = *.yangjibao.com
 
 *******************************/
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const vip = '/account';
-
-
-if (url.indexOf(vip) != -1) {
-    obj.data.is_pay = true;
-    obj.data.vip_expiry_date = "2099-12-31";
-    obj.data.vip_label = true;
-
-	body = JSON.stringify(obj);
-}
+var body = $response.body.replace(/vip_label":false/g, 'vip_label":true')
+						 .replace(/vip_expiry_date":null/g, 'vip_expiry_date":"2099-12-31"')
+						 .replace(/is_pay":false/g, 'is_pay":true');
+$done({ body });
